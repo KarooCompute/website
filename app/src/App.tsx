dfa5c63098abbd9299c9f48956518dfab6f1a1ac
Kaleb Bruwer <kaleb.bruwer@gmail.com>
2026-02-24 16:14:32 +0200

    More tweaks

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -8,7 +8,7 @@ import './App.css'
 
 function App() {
   return (
-    <div>
+    <div className="app-layout">
       <Navbar />
       <main>
         <Routes>
--- app/src/pages/Rose.tsx
+++ app/src/pages/Rose.tsx
@@ -4,10 +4,11 @@ function Rose() {
       <div className="container content-section">
         <h1 className="rose-color-light">Rose: Rosetta Of Software Engineering</h1>
         <p>
-          Rose is an LSP-based semantic translation tool being developed by Karoo Compute Solutions to support the rapid translation of software from one language to another.
+          Rose is an LSP-based Semantic Extraction tool being developed by Karoo Compute Solutions to support the rapid translation of software from one language to another.
         </p>
         <p>
           Through the use of Semantic Extraction, Rose can check the equivalence of two codebases, providing useful diagnostic information for any discrepancies.
+          This is to be used in conjunction with an LLM to automate the translation process.
         </p>
         <p>
           Rose is still under development, and not publicly available yet.
